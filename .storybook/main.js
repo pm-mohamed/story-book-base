05b7f0b054796235fd5802806984fb0874c244b6
Mohamed Hassan <mohamed.hassan@pmagentur.com>
2022-01-27 16:19:34 +0100

    added sass config

--- .storybook/main.js
+++ .storybook/main.js
@@ -4,9 +4,25 @@ module.exports = {
       "../stories/**/*.stories.@(js|jsx|ts|tsx)"
     ],
     "addons": [
+    {
+      name: '@storybook/preset-scss',
+      options: {
+        cssLoaderOptions: {
+           modules: true,
+        },
+        sassLoaderOptions: {
+          additionalData: (content) => {
+            // paths are relative to root dir in this case
+            return `
+              @import '../main.scss';
+            `
+          }
+        }
+      }
+    },
       "@storybook/addon-links",
       "@storybook/addon-essentials",
-      '@whitespace/storybook-addon-html'
+      '@whitespace/storybook-addon-html',
     ],
     "framework": "@storybook/html"
 }
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,5 @@
+import '!style-loader!css-loader!sass-loader!../main.scss'
+
 export const parameters = {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
